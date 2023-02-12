c296bb2d867aec11779ca134200dd40105959e62
sourav4125 <souravmisra4125@gmail.com>
2023-02-12 09:36:48 +0530

    cron job

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const cron = require('node-cron');
+const events = [
+  { text: "textOne", dateTime: "5 09 22 11 2 6" },
+  { text: "textTwo", dateTime: "30 10 21 12 2 0" },
+  { text: "textThree", dateTime: "30 15 23 13 2 1" },
+  { text: "textFour", dateTime: "30 15 23 14 2 2" },
+  { text: "textFive", dateTime: "30 15 23 15 2 3" },
+  { text: "textSix", dateTime: "30 15 23 16 2 4" },
+  { text: "textSeven", dateTime: "30 15 23 17 2 5" },
+  { text: "textEight", dateTime: "30 15 23 18 2 6" },
+  { text: "textNine", dateTime: "30 15 23 19 2 0" },
+  { text: "textTen", dateTime: "30 15 23 20 2 1" }
+];
+
+
+const trigger = (text) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(text.split("").reverse().join(""));
+    }, 7000);
+  });
+};
+
+const scheduler = function () {
+  for (const event of events) {
+    cron.schedule(event.dateTime, async () => {
+      const result = await trigger(event.text);
+      console.log(`text: ${result}`);
+      let currentDate = new Date();
+      let dateTime = currentDate.getFullYear() + "-"
+        + (currentDate.getMonth() + 1) + "-"
+        + currentDate.getDate() + " "
+        + currentDate.getHours() + ":"
+        + currentDate.getMinutes() + ":"
+        + currentDate.getSeconds() + "."
+        + currentDate.getMilliseconds();
+      console.log(`dateTime: ${dateTime}`);
+    });
+  }
+};
+
+scheduler();
